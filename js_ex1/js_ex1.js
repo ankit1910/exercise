08619c50ab2fdf3d246547042ee90999b3507e1a
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-01 18:07:45 +0530

    javascript exercise 1

--- js_ex1/js_ex1.js
+++ js_ex1/js_ex1.js
@@ -1,25 +1,29 @@
-function check(arg,status) 
+function check(arg,status)                        // function to check and uncheck all
 {
   var check_box= new Array(); 
-  check_box= document.getElementsByName(arg);
-  
-  if(status==true)
+  check_box= document.getElementsByName(arg);               
+  if(status==true)                                //for checking all checkbox
   {
-  for (var i=0; i<check_box.length; i++)  
-   {
-    if (check_box[i].type == 'checkbox')   
-     {check_box[i].checked = true;}
-   }
- }
-  else if(status==false)
+    for (var i=0; i<check_box.length; i++)        //traversing each child of list
     {
-  for (var i=0; i<check_box.length; i++)  
-   {
-    if (check_box[i].type == 'checkbox')   
-     {check_box[i].checked = false;}
-   }
+      if (check_box[i].type == 'checkbox')   
+      { 
+        check_box[i].checked = true;
+      }
+    }
+  }
+  else if(status==false)                         //for checking all checkbox
+  {
+    for (var i=0; i<check_box.length; i++)       //traversing each child of list
+    {
+      if (check_box[i].type == 'checkbox')   
+      {
+        check_box[i].checked = false;
+      }
+    }
   }
   else
-      {alert("error checked value out of domain")}
-   
+  {
+    alert("error checked value out of domain")
+  }   
 }
