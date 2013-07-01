4e55abb8247b1d9fd3b6b27778dd19f27080c470
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-01 17:15:19 +0530

    javascript exercise 2

--- js_ex2/js_ex2.js
+++ js_ex2/js_ex2.js
@@ -1,35 +1,35 @@
-function check(obj)
+function check(obj)       // function to check atmost three checkbox
 {
-  document.getElementsByName('cn')[0].checked=false;
-  var count=0, disp= " are ", check_sel=document.getElementsByName('color');
+  document.getElementsByName('cn')[0].checked = false;        // deselect checknone onchecking others
+  var count = 0, disp = " are ", check_sel = document.getElementsByName('color');
   
-for (var i = 0; i<check_sel.length; i++)
-{ 
-if(check_sel[i]!= obj)
- {
-  if (check_sel[i].checked==true && count++<=2) 
-  {
-    disp+=check_sel[i].getAttribute('value');
-    disp+= " ";
+  for (var i = 0; i < check_sel.length; i++)
+  { 
+    if(check_sel[i] != obj)
+    {
+    if (check_sel[i].checked == true && count ++<= 2) 
+    {
+      disp += check_sel[i].getAttribute('value');
+      disp += " ";
+    }
+    }
   }
- }
-}
 
- if(count>=3)
-  { alert("you can't select more than 3 check box.\n you already had seleted:" + disp);
-    obj.checked=false;
+  if(count >= 3)           //alert when there is attempt to check the 4th checkbox
+  {
+    alert("you can't select more than 3 check box.\n you already had seleted:" + disp);
+    obj.checked = false;
   } 
 }
 
-function checknone(arg)
- {
-  var check_box= document.getElementsByName(arg);
-
-for (var i=0; i<check_box.length; i++)  
- { 
-  if (check_box[i].type == 'checkbox')   
-   {
-    check_box[i].checked = false;
-   }
- }
+function checknone(arg)   //funtion for deseleting all the checkboxes
+{
+  var check_box = document.getElementsByName(arg);
+  for (var i = 0; i < check_box.length; i++)  
+  { 
+    if (check_box[i].type == 'checkbox')   //checking object is checkbox type or not
+    {
+      check_box[i].checked = false;
+    }
+  }
 }
