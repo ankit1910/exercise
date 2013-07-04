b390951e7093fb1ddd6fcd6516472eb46471d604
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-05 02:30:53 +0530

    js exercise check all/none

--- js_ex1/js_ex1.js
+++ js_ex1/js_ex1.js
@@ -2,21 +2,19 @@
 // function to check and uncheck all
 // arg hold name of checkbox 
 // status hold the boolean value upon which function has to be performed
-function invert_sel(arg,status)                        
+function invert_sel(nameCheck, status)                        
 {
-  var check_box = [];
-  var check_box = document.getElementsByName(arg);               
+  var check_box = {};
+  var check_box = document.getElementsByName(nameCheck);               
   //for traversing each child
-  for (var i = 0; i < check_box.length; i++)        
-  {
+  for(var i = 0; i < check_box.length; i++) {
     //for checking type of nodes
-    if (check_box[i].type == 'checkbox')   
-    { 
+    if(check_box[i].type == 'checkbox') { 
       check_box[i].checked = status;
     }
-    else
-    {
+    else{
       alert("error--'checked value out of domain'")
     }   
   }
+  return false;
 }
