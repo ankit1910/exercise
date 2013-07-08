04b4f7bbd7dee2d3e46781771fccfc847be0c36b
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-08 18:18:44 +0530

    js exercise disply prompt

--- displayPromt/displayPromt.js
+++ displayPromt/displayPromt.js
@@ -1,5 +1,7 @@
+//function with call on loading of window
 window.onload = function(){
   var flag;
+  // repeat until we get the proper first name
   do{
     flag = true;
     var fName = prompt("Please enter your first name");
@@ -9,7 +11,7 @@ window.onload = function(){
     }
   }
   while(flag != true);
-
+  // repeat until we get the proper last name
   do{
     flag = true;
     var lName = prompt("please enter your last name");
@@ -19,7 +21,8 @@ window.onload = function(){
     }
   }
   while(flag != true);
+  //for checking that user click cancel on prompt window
   if(fName != null || lName != null){
- document.getElementById("display").innerHTML="HELLO " + fName + "  " + lName;
+    document.getElementById("display").innerHTML="HELLO " + fName + "  " + lName;
   }
 }
