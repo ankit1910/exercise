b3fe7fd64f49d4e05a4c212054587153e5ed3325
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-09 18:29:53 +0530

    js exercise valid Numeric Value

---
+++ validNumericValue/validNumericValue.js
@@ -0,0 +1,15 @@
+var validateForm = document.getElementById('formSubmit');
+//adding event handler on submitting the form...
+validateForm.addEventListener("submit", checkFormValidate);
+
+function checkFormValidate(event){
+	var regex = /^[0-9]+$/;
+  if(regex.test(document.getElementById('input').value)) {
+    document.getElementById('output').value = 'true';
+    alert("submitting form");
+  }
+  else{
+    document.getElementById('output').value = 'false';
+    event.preventDefault();
+  }
+}
