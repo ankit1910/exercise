c43c3625ed4608a5c8034d55819c20b076881957
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-11 13:16:56 +0530

    js exercise displayPromt

--- displayPromt/displayPromt.js
+++ displayPromt/displayPromt.js
@@ -1,28 +1,23 @@
 //function with call on loading of window
 window.onload = function(){
-  var flag;
+  var flag, fName = "", lName = "";
   // repeat until we get the proper first name
-  do{
-    flag = true;
-    var fName = prompt("Please enter your first name");
-    if(fName == "" || fName.match(" ")){
-      alert("enter a valid first name");
-      flag = false;
-    }
-  }
-  while(flag != true);
-  // repeat until we get the proper last name
-  do{
-    flag = true;
-    var lName = prompt("please enter your last name");
-    if(lName == "" || lName.match(" ")){
-     alert("enter a valid last name");
-     flag = false;
-    }
-  }
-  while(flag != true);
+  fName = promptName("first");
+  lName = promptName("last");
+
+  function promptName(obj){
+    do{
+      flag = true;
+      var Name = prompt("Please enter your " + obj + " name");
+        if(Name.trim() == ""){
+        alert("enter a valid " + obj + " name");
+        flag = false;
+      }
+    }while(flag != true);
+    return Name;
+  } 
   //for checking that user click cancel on prompt window
   if(fName != null || lName != null){
-    document.getElementById("display").innerHTML="HELLO " + fName + "  " + lName;
+    document.write("HELLO " + fName + "  " + lName);
   }
 }
