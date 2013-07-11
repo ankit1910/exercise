db8765b9776b0396e8c73fea9fbeb4d1b050b126
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-11 14:18:50 +0530

    js exercise moveCountry

--- moveCountry/moveCountry.js
+++ moveCountry/moveCountry.js
@@ -1,34 +1,21 @@
-var add = document.getElementById('add');
-var remove = document.getElementById('remove');
+var addCountry = document.getElementById('add');
+var removeCountry = document.getElementById('remove');
 
 //moving item from first selectbox to second selectbox...
-add.addEventListener( "click", addToSecond);
-
-function addToSecond(){
-  //getting value of the selected option item	
-  var firstBox = document.getElementById('first_box');
-  var selectOption = firstBox.options[firstBox.selectedIndex];
-  //adding that selected item to second select box...
-  var secondBox = document.getElementById('second_box');
-  var newOption = document.createElement("option");
-  newOption.appendChild(document.createTextNode(selectOption.value));
-  secondBox.appendChild(newOption);
-  // removing from first checkbox
-  firstBox.remove(firstBox.selectedIndex);
-}
+addCountry.addEventListener( "click", function(){
+  swapCountry('first_box', 'second_box');
+});
 
 //moving item from second selectbox to first selectbox...
-remove.addEventListener( "click", addToFirst);
-
-function addToFirst(){
-  //getting value of the selected option item	
-  var secondBox = document.getElementById('second_box');
-  var selectOption = secondBox.options[secondBox.selectedIndex];
-  //adding that selected item to first select box.. 
-  var firstBox = document.getElementById('first_box');
-  var newOption = document.createElement("option");
-  newOption.appendChild(document.createTextNode(selectOption.value));
-  firstBox.appendChild(newOption);
-  //removing from second checkbox..
-  secondBox.remove(secondBox.selectedIndex);
+removeCountry.addEventListener( "click", function(){
+  swapCountry('second_box', 'first_box');
+});
+//swaping the country from one box to another
+function swapCountry(box_from, box_to) {
+  var boxFrom = document.getElementById(box_from);
+  var boxTo = document.getElementById(box_to);
+  while (boxFrom.selectedIndex != -1) {
+    boxTo.appendChild(boxFrom.options[boxFrom.selectedIndex]);
+  }
 }
+
