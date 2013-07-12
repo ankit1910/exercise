ee6ac8cb758678632605903cda4d72c1765be378
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-12 17:38:39 +0530

    validate_form

--- validate_form/validate_form.js
+++ validate_form/validate_form.js
@@ -1,44 +1,22 @@
 //defining an object with multiple variables
 var validateForm = document.getElementById("validateForm");
-var formFields = {  
-  login: document.getElementById("login"),
-  email: document.getElementById("email"),
-  name:  document.getElementById("name"),
-  homePage: document.getElementById("homepage"),
-  textArea: document.getElementById("aboutme"),
-  recNotif: document.getElementById("notification")
-}
 //adding event handler
 validateForm.addEventListener("submit", validFormFields);
-//regular expressions
-var testEmail = /^(?:\w+\.?)*\w+@(?:\w+\.?)*\w{2,4}$/;
-var testHome = /^[a-z0-9._%-]+\.[a-z]{2,4}$/;
-
-//function to validate form
+var formFields = document.getElementsByClassName('field');
 function validFormFields(e){
-  if(formFields.login.value == "" || formFields.login.value.match(" ")) {
-    alert("login id is not correct");
+    for(var i = 0; i < 4; i++){
+      if(formFields[i].value.trim() == ""){
+      alert("please check " + formFields[i].id + " field");
+      e.preventDefault();
+      return ;
+      }
+    }
+  if(document.getElementById('aboutme').value.trim() < 50){
+    alert(" the length of about me field should be atleast 50");
+    e.preventDefault();
+    return ;
   }
-  if(formFields.email.value.trim() == ""){
-    alert("you forgot the email id");
+  if(!document.getElementById('notification').checked){
+    alert("are you sure you dont want to recieve notification");
+    e.preventDefault();
   }
-  else if(!testEmail.test(formFields.email.value)){
-    alert("please check email id");
-  }  
-  if(formFields.name.value.trim() == "") {
-    alert("please enter name");
-  }
-  if(formFields.homePage.value.trim() == ""){
-    alert("you forgot the home page url");
-  }
-  else if(!testHome.test(formFields.homePage.value)){
-    alert("please check homepage url");
-  }
-  if (formFields.textArea.value.trim().length <= 50 ) {
-    alert("description should be in min 50 words");
-  }
-  if(formFields.recNotif.checked != true) {
-    alert("you dont want to recieve notification?\n sure?");
-  }
-  e.preventDefault();
-}
