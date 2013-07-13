eafe76364bafcfd21cccdea0f6bb3aed8c9130fb
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-07-13 14:39:46 +0530

    validate_form

--- validate_form/validate_form.js
+++ validate_form/validate_form.js
@@ -1,23 +1,25 @@
-
 var validateForm = document.getElementById("validateForm");
 //adding event handler
 validateForm.addEventListener("submit", validFormFields);
 var formFields = document.getElementsByClassName('field');
 function validFormFields(e){
-    for(var i = 0; i < 4; i++){
-      if(formFields[i].value.trim() == ""){
+  var flag = true;
+  for(var i = 0; i < formFields.length; i++){
+    if(formFields[i].value.trim() == ""){
       alert("please check " + formFields[i].id + " field");
       e.preventDefault();
       return ;
-      }
     }
-  if(document.getElementById('aboutme').value.trim() < 50){
+  }
+  if(document.getElementById('aboutmee').value.length <= 50){
     alert(" the length of about me field should be atleast 50");
     e.preventDefault();
     return ;
   }
   if(!document.getElementById('notification').checked){
-    alert("are you sure you dont want to recieve notification");
-    e.preventDefault();
+    var confirmNotif = confirm("are you sure you dont want to recieve notification");
+    if(!confirmNotif){
+      e.preventDefault();
+    }
   }
 }
